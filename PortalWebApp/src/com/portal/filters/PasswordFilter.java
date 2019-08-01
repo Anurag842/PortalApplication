0bb623e10d1fb3f1e7c794e737dd7a9d7785f36f
anurag.dutta <anurag.dutta@IRIS-CSG-1212.irissoftware.com>
2019-08-01 16:56:32 +0530

    Added filter for logging in only from 9 am to 5pm

---
+++ PortalWebApp/src/com/portal/filters/PasswordFilter.java
@@ -0,0 +1,33 @@
+package com.portal.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+
+@WebFilter("/validate")
+public class PasswordFilter implements Filter {
+
+  
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		
+	}
+
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ PortalWebApp/src/com/portal/filters/TimeFilter.java
@@ -0,0 +1,53 @@
+package com.portal.filters;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebFilter("/Homepage.jsp")
+public class TimeFilter implements Filter {
+
+ 
+
+	
+	public void destroy() {
+		System.out.println("destroyr");
+	}
+
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req=(HttpServletRequest)request;
+		HttpServletResponse res=(HttpServletResponse)response;
+		PrintWriter out=res.getWriter();
+		
+		LocalDateTime ldt=LocalDateTime.now();
+		int hr=ldt.getHour();
+		System.out.println("dofilter");
+		
+		if(hr>=9 && hr<=17)
+		{
+			chain.doFilter(request, response);
+		}
+		else
+		{
+			res.sendRedirect("failure.jsp");
+		}
+	}
+
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("init");
+	}
+
+}
